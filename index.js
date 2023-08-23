3d6eb50a856c555fcc4254a313687ee950e4a8b3
Mark Arthur <markjefferson.arthur@telus.com>
2023-08-22 20:47:03 -0400

    Feat(Dummy): Create a dummy API to return pitcher percent

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/predict', (req, res) => {
+
+    const batID = req.query.batID
+    const pitcherID = req.query.pitcherID
+    const batAvg = parseFloat(req.query.batAvg)
+    const pitchAvg = parseFloat(req.query.pitchAvg)
+
+    res.json( { predictionPercent: (batAvg + pitchAvg) } )
 })
 
 app.listen(port, () => {
